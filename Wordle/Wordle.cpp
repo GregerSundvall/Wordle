fc4903f509a57a542b008a38f7a9842e5ba80493
Greger Sundvall <gsundvall@gmail.com>
2022-03-01 16:24:19 +0100

    getting started

--- Wordle/Wordle.cpp
+++ Wordle/Wordle.cpp
@@ -2,35 +2,72 @@
 
 #include <fstream>
 #include <iostream>
-#include <map>
 #include <random>
 #include <string>
 #include <Windows.h>
 #include <stdio.h>
+#include <array>
 
-char getColoredChar(char c, std::string color)
+std::string getColoredChar(char c, int correct)
 {
-    std::map<std::string, std::string> colors ={
-        {"green", "42"},
-        {"red", "42"},
-        {"blue", "42"}};
+    std::string base_string = "\x1b[";
+    switch (correct)
+    {
+        case 0: {base_string += "m"; break;}
+        case 1: {base_string += "32m"; break;}
+        case 2: {base_string += "30;42m"; break;}
+    }
+    base_string += c;
+    return base_string += "\r";
 }
+
+
+#define FOREGROUND(color, text) "\x1B[" << static_cast<int>(color) << "m" << text << "\033[0m"
+#define BACKGROUND(color, text) "\033[3;42;" << static_cast<int>(color) << "m" << text << "\033[0m"
+
+enum class ForegroundColor : int {
+    Red = 31,
+    Green = 32,
+    Yellow = 33,
+    BrightRed = 91,
+    BrightGreen = 92,
+    BrightYellow = 93
+};
+
+enum class BackgroundColor : int {
+    Red = 41,
+    Green = 42,
+    Yellow = 43,
+    BrightRed = 101,
+    BrightGreen = 102,
+    BrightYellow = 103
+};
 
 
 int main()
 {
     std::string secret;
-    std::string guess;
     std::string words;
+    std::array<std::string, 6> guesses;
 
-    // Setting up for console colors
+    // Setting up for console colors -----------------------------------------
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
     if (hOut == INVALID_HANDLE_VALUE) return GetLastError();
     DWORD dwMode = 0;
     if (!GetConsoleMode(hOut, &dwMode)) return GetLastError();
     dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
     if (!SetConsoleMode(hOut, dwMode)) return GetLastError();
+
+    std::cout << FOREGROUND(ForegroundColor::BrightRed, "Hello world!") << std::endl;
+    std::cout << BACKGROUND(BackgroundColor::BrightRed, "Hello world!") << std::endl;
+    std::cout << BACKGROUND(BackgroundColor::Yellow, "AB")
+        << BACKGROUND(BackgroundColor::Green, "C") << BACKGROUND(BackgroundColor::Red, "DE") << std::endl;
+
+    wprintf(L"\x1b[32mGreen on black\r\n");
+    wprintf(L"\x1b[30;42mBlack on green\r\n");
+    wprintf(L"\x1b[mReset to white on black\r\n");
     
+    // Reading file ---------------------------------------------------------
     std::ifstream file("words.txt");
     if (file.is_open())
     {
@@ -54,41 +91,50 @@ int main()
     }
     file.close();
 
+    // Picking random word -------------------------------------------------------
     std::random_device rand_dev;
     std::mt19937 generator(rand_dev());
     std::uniform_int_distribution<int> distribution(0, 8597);
     int randomRangedInt = distribution(generator);
-    secret = words.substr( randomRangedInt * 6, 5);
+    // secret = words.substr( randomRangedInt * 6, 5);
+    secret = "level";
 
+    // Game start ----------------------------------------------------------------
     std::cout << "What is your first guess?" << std::endl;
-    std::cin >> guess;
+    std::cin >> guesses[0];
 
-    for (int i = 0; i < 5; ++i)
+    for (int round = 1; round < 7; ++round)
     {
-        if (secret[i] == guess[i])
+        for (int charPos = 0; charPos < 5; ++charPos)
         {
-            std::cout <<guess.at(i);
-        }
-        else
-        {
-            for (int j = 0; j < 5; ++j)
+            std::string guess = guesses[charPos];
+            if (guess[charPos] == secret[charPos])
             {
-                if (j == i) continue;
-                if (guess[i] == secret[j])
+                std::cout << getColoredChar(guess[charPos], 2);
+                std::cout << "test";
+            }
+            else
+            {
+                // Check against other letters in word
+                bool match = false;
+                for (int j = 0; j < 5; ++j)
                 {
-                    std::cout <<guess.at(i);
+                    if (j == charPos) continue; // do not check letter in same location as self
+                    if (guess[charPos] == secret[j])
+                    {
+                        match = true;
+                    }
                 }
-                else
-                {
-                    std::cout << guess[i];
-                }
+                
+                std::cout << getColoredChar(guess[charPos], match ? 1 : 0);
+                
             }
         }
-        if (i < 4)
+        if (round < 7)
         {
             std::cout << std::endl;;
             std::cout << "OK, next guess:";
-            std::cin >> guess;
+            std::cin >> guesses[round +1];
         }
         else
         {

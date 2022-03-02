567ca722e0bc788ad9ddf91564d2717e0aba7a4e
Greger Sundvall <gsundvall@gmail.com>
2022-03-02 14:44:00 +0100

    replace all printf's with cout

--- Wordle/Wordle.cpp
+++ Wordle/Wordle.cpp
@@ -16,32 +16,23 @@ int main()
     bool play = true;
     
     // Setting up for console colors -----------------------------------------
-    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hOut == INVALID_HANDLE_VALUE) return GetLastError();
-    DWORD dwMode = 0;
-    if (!GetConsoleMode(hOut, &dwMode)) return GetLastError();
-    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-    if (!SetConsoleMode(hOut, dwMode)) return GetLastError();
-    // wprintf(L"\x1b[32mGreen on black\r\n");
-    // wprintf(L"\x1b[30;42mBlack on green\r\n");
-    // wprintf(L"\x1b[mReset to white on black\r\n");
+    HANDLE h_out = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (h_out == INVALID_HANDLE_VALUE) return GetLastError();
+    DWORD dw_mode = 0;
+    if (!GetConsoleMode(h_out, &dw_mode)) return GetLastError();
+    dw_mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    if (!SetConsoleMode(h_out, dw_mode)) return GetLastError();
     
     // Reading file ---------------------------------------------------------
     std::ifstream file("words.txt");
     if (file.is_open())
     {
-        char currentchar;
+        char current_char;
         while (file)
         {
-            currentchar = file.get();
-            words += currentchar;
+            current_char = file.get();
+            words += tolower(current_char);
         }
-        // std::string currentLine;
-        // while (file)
-        // {
-        //     getline(file, currentLine);
-        //     words.append(currentLine);
-        // }
     }
     else
     {
@@ -49,6 +40,8 @@ int main()
     }
     file.close();
 
+    std::cout << "Five letter word. Six guesses. You got this." << std::endl;
+    // Game loop -----------------------------------------------------------------
     while (play)
     {
         std::string secret;
@@ -59,41 +52,41 @@ int main()
         std::random_device rand_dev;
         std::mt19937 generator(rand_dev());
         std::uniform_int_distribution<int> distribution(0, 8597);
-        int randomRangedInt = distribution(generator);
-        // secret = words.substr( randomRangedInt * 6, 5);
-        secret = "level";
+        int random_ranged_int = distribution(generator);
+        secret = words.substr( random_ranged_int * 6, 5);
 
         // Game start ----------------------------------------------------------------
         for (int round = 0; round < 6; ++round)
         {
-            std::cout << "\x1b[mWhat's your guess?\r" << std::endl;
+            std::cout << "\n" << "\x1b[mWhat's your guess? ";
             std::cin >> guesses[round];
-            int correctChars = 0;
+            int correct_chars = 0;
         
-            for (int charPos = 0; charPos < 5; ++charPos)
+            for (int char_pos = 0; char_pos < 5; ++char_pos)
             {
                 std::string guess = guesses[round];
-                if (guess[charPos] == secret[charPos])
+                if (guess[char_pos] == secret[char_pos])
                 {
-                    correctChars ++;
-                    printf("\x1b[30;42m%hc\033[0m", guess[charPos]);
+                    correct_chars ++;
+                    std::cout << "\x1b[30;42m" << guess[char_pos] << "\033[0m";
                 }
                 else
                 {
                     // Check against other letters in word
-                    bool match = false;
+                    bool letter_is_in_word = false;
                     for (int j = 0; j < 5; ++j)
                     {
-                        if (j == charPos) continue; // do not check letter in same location as self
-                        if (guess[charPos] == secret[j])
+                        if (j == char_pos) continue; // do not check letter in same location as self
+                        if (guess[char_pos] == secret[j])
                         {
-                            match = true;
+                            letter_is_in_word = true;
                         }
                     }
-                    match ? printf("\x1b[92m%hc\033[0m", guess[charPos]) :
-                            printf("\033[37;40m%hc\033[0m", guess[charPos]);
+                    
+                    letter_is_in_word ? std::cout << "\x1b[30;43m" << guess[char_pos] << "\033[0m" :
+                            std::cout << "\x1b[30;48;2;200;200;200m" << guess[char_pos] << "\033[0m";
                 }
-                if (correctChars == 5)
+                if (correct_chars == 5)
                 {
                     has_won = true;
                 }
@@ -107,17 +100,18 @@ int main()
 
         if (has_won)
         {
-            printf("You nailed it!\n");
+            std::cout << "You nailed it!" << std::endl;
         }
         else
         {
-            printf("Sorry to see you humiliate yourself like that. Better luck next time.\n");
+            std::cout << "Sorry to see you humiliate yourself like that." << std::endl;
+            std::cout << "The word was " << secret << ". Better luck next time." << std::endl;
         }
 
-        printf("Play again? y/n\n");
-        char playAgain;
-        std::cin >> playAgain;
-        if (playAgain != 'y') play = false;
+        std::cout << "Play again? (y/n)" << std::endl;
+        char play_again;
+        std::cin >> play_again;
+        if (play_again != 'y') play = false;
     }
         
     return 0;

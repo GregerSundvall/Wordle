1dc383d9e1d55d8ace076cf77e6a17f90d0fe76b
Greger Sundvall <gsundvall@gmail.com>
2022-03-08 20:42:39 +0100

    Getting started on custom container

--- Wordle/words.h
+++ Wordle/words.h
@@ -3,6 +3,23 @@
 #include <string>
 
 
+class word {
+    char word[5];
+    bool used;
+};
+
+
+class word_list {
+
+};
+
+// Vector-ish class with tuples of char[5] and a bool.
+// Custom tuple class with char[5] and a bool will minimize allocation.
+// Resizeable to be able to add more words.
+// When 90% of items are false, set all back to true.
+// Allocate for 10000 items first. If full, MOVE all to new "vector" 50% bigger.
+
+
 
 class words
 {

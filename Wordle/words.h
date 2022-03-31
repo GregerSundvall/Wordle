51629d7060b83bc99c70e06057f2bf7f2e3590c8
MaxTaranov <kingofbeavers@gmail.com>
2022-04-01 00:07:06 +0200

    Added feedback

--- Wordle/words.h
+++ Wordle/words.h
@@ -19,16 +19,18 @@ class word_list {
 // When 90% of items are false, set all back to true.
 // Allocate for 10000 items first. If full, MOVE all to new "vector" 50% bigger.
 
-
+// feedback: while I see the effort to add a custom words storage I'm not sure I fully understand the intent behind it.
+// You use a wrapper around the std::map, however it doesn't seem like you ever set or get values(ints) in the map but only use keys(strings).
+// Moreover get_random() has to iterate the map which is far from ideal(see my comment in that function)
 
 class words
 {
     std::map<std::string, int> list_;
 public:
     
-    void add_word(std::string word);
+    void add_word(std::string word); // feedback - argument could be std::string& instead to avoid copying
 
-    void import_from_file(std::string filename);
+    void import_from_file(std::string filename); // feedback - same here
 
     std::string get_random();
 
